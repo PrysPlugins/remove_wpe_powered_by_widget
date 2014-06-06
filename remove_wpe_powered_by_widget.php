1d82b831c612415bb1ecee49de85fdb55387031f
Jeremy Pry <JPry@users.noreply.github.com>
2014-06-06 16:33:14 -0400

    Create the main plugin file

---
+++ remove_wpe_powered_by_widget.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Remove WPE Powered By Widget
+ * Plugin URI: https://github.com/PrysPlugins/remove_wpe_powered_by_widget
+ * Description: Remove the "Powered by WP Engine" widget
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
+
+add_action( 'widgets_init', 'jpry_remove_wpe_widget' );
+/**
+ * Unregister the WP Engine "Powered By" widget.
+ *
+ * @since 1.0
+ */
+function jpry_remove_wpe_widget() {
+	wp_unregister_sidebar_widget( 'wpe_widget_powered_by' );
+}
